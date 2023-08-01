cd6c6d7912c7de1e82320765a2cc4c1fd383a450
andylamax <andylamax@programmer.net>
2023-08-02 02:56:36 +0300

    Working on Welcom screen so that we get to registration

--- brand/src/commonMain/kotlin/asterina/Welcome.kt
+++ brand/src/commonMain/kotlin/asterina/Welcome.kt
@@ -1,27 +1,18 @@
 package asterina
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 @Composable
-fun Splash(
-    logo: Char,
-    label: String,
-    timeout: Duration = 3.seconds,
-    onCompleted: () -> Unit
+fun Welcome(
+    text: String = "Welcome",
+    onGetStartedClicked: () -> Unit
 ) = Surface(
     modifier = Modifier.fillMaxSize(),
     color = if (ModeProvider.current is Mode.Light) {
@@ -30,22 +21,21 @@ fun Splash(
         PrimaryPaletteProvider.current.c900
     },
 ) {
-    LaunchedEffect(Unit) {
-        delay(timeout)
-        onCompleted()
-    }
-    Box(modifier = Modifier.align(Alignment.Center).padding(bottom = 80.dp)) {
-        Box(modifier = Modifier.align(Alignment.Center)) {
-            Logo(logo)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.matchParentSize().debug()
+    ) {
+        Heading1(
+            text = text,
+            size = Size.XXLarge,
+            align = TextAlign.Center,
+            modifier = Modifier.weight(0.1f).debug()
+        )
+        Box(modifier = Modifier.weight(0.8f).debug()) {
+            Heading3("test2")
         }
-        Column(modifier = Modifier.align(Alignment.BottomCenter)) {
-            Heading1(
-                text = label,
-                size = Size.XXXLarge,
-                align = TextAlign.Center,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-            Spacer(modifier = Modifier.height(65.dp))
+        Box(modifier = Modifier.weight(0.1f).debug()) {
+            Heading3("test3")
         }
     }
 }
